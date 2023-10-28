eac2ca70ae643c644cbc5608903d3716223c728a
choivember <pocketmongyo@gmail.com>
2023-10-28 16:39:41 +0900

    Revert "로그인 첫번째창 구현"
    This reverts commit 901f98631ce54bd6bcab31a0defadd98c9d0ef3a.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion';
+import logo from './logo.svg';
 import './App.css';
-import LoginPage from "./LoginPage/LoginPage";
 
 function App() {
   return (
-    <BrowserRouter>
-            <AnimatePresence>
-                <Routes>
-                    <Route index element={<LoginPage/>}/>
-                </Routes>
-            </AnimatePresence>
-        </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
