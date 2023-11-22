9fb3ee626ed94818929c8283fdad6d28eb7e1174
choivember <pocketmongyo@gmail.com>
2023-11-22 18:21:36 +0900

    컴포넌트 모달태그 추가

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import style from './LoginPage.module.css';
 import { Link } from 'react-router-dom';
+import { Modal } from 'react-bootstrap';
 
     function Content() {
 
@@ -11,33 +12,36 @@ import { Link } from 'react-router-dom';
         );
     }
 
-    function Kakao() {
-
-        return (
-            <div>
-                <Link to={'/KakaoLogin'}>
-                    <p className={style.kakao}>Login with kakao</p>
-                </Link>
-            </div>
-        )
-    }
-
     function Login() {
+
+        const [showModal, setShowModal] = useState(false);
+        const handleCloseModal = () => setShowModal(false);
+
+        useEffect(() => {
+            setShowModal(true);
+        }, []);
+
         return(
-            <>
-                <div className={style.login}>
-                    <h3>Sign in to 두유타운</h3>
-                    <Kakao/>
-                </div>
-            </>
+            <div className={style.login}>
+                <Modal.Dialog show={showModal} onHide={handleCloseModal}>
+                    <Modal.Header><br></br>
+                        <h3>Sign in to 두유타운</h3>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Link to={'/KakaoLogin'}>
+                            <p className={style.kakao}>Login with Kakao</p>
+                        </Link>
+                    </Modal.Body>
+                </Modal.Dialog>
+            </div>
         );
     }
 
     function LoginPage(props) {
         return (
-            <div className={style.container}>
+            <>
                 <Content/>
-            </div>
+            </>
         );
     }
 
