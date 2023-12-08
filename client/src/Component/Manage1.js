afa98c87843bd4abb56f5f2442abcc4c6e560afe
choivember <pocketmongyo@gmail.com>
2023-12-08 16:12:23 +0900

    동아리 신청폼 구현

--- client/src/Component/Manage1.js
+++ client/src/Component/Manage1.js
@@ -6,8 +6,8 @@ import Content1 from '../Common/Content1';
 function MyPage() {
     return (
             <div className={style.content}>
-                
-                <Navbar2/><Content1/>
+                <Navbar2/>
+                <Content1/>
             </div>
     );
 }
