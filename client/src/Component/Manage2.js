2d4aed3dbfa03971b46d2f30a5268619b3603856
choivember <pocketmongyo@gmail.com>
2023-12-11 21:58:29 +0900

    히든타입 추가

---
+++ client/src/Component/Manage2.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import Navbar2 from '../Common/Navbar2';
+import Content2 from '../Common/Content1';
+
+function MyPage() {
+    return (
+            <>
+                <Navbar2/>
+                <Content2/>
+            </>
+    );
+}
+
+export default MyPage;
